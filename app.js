5bc45e91fd33e5d8dd2aa5fc5026c5f60d552e27
nsznsnb <nsznsnb@gmail.com>
2021-12-26 23:26:38 +0900

    定時送信の部分を一部実装

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const axios = require("axios");
+const cron = resuire("node-cron")
 
 const url = "https://www.jma.go.jp/bosai/forecast/data/forecast/";
-const area = "460100"; // 鹿児島市
+const area = "460100"; // 鹿児島県内
 
-(async function getWeatherForecast() {
-  try {
-    const response = await axios.get(`${url}${area}.json`);
-    for(const area of response.data[0].timeSeries[0].areas){
+// 毎日定刻に送信を行う
+cron.schedule("", () => {
+  /**
+   * 気象庁からデータを取得してLINEに配信
+   */
+  (async function getWeatherForecast() {
+    try {
+      const response = await axios.get(`${url}${area}.json`);
+      for (const area of response.data[0].timeSeries[0].areas) {
         console.log(`----${area.area.name}----`);
-        for(const weather of area.weathers){
-            console.log(weather);
+        for (const weather of area.weathers) {
+          console.log(weather);
         }
+      }
+    } catch (error) {
+      console.error(error);
     }
-  } catch (error) {
-    console.error(error);
-  }
-})();
+  })();
+});
